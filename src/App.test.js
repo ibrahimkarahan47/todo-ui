5658cab0999996f873b5b93f4d85b1cecd950b62
İbrahim Karahan <ibrahim.karahan@getir.com>
2021-02-12 19:52:30 +0300

    [MD-1][feat] Implement app component.

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,10 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { shallow } from 'enzyme';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App component', () => {
+	it('should render a <div /> without crashing', () => {
+		const container = shallow(<App />);
+		expect(container.find('div').length).toEqual(1);
+  });
 });
